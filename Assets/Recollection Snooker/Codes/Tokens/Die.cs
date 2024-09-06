b92463502591ae54f2a1286ec3ec084fd13e5b2a
CDiegoSV <cdsalgadov@gmail.com>
2024-09-05 19:01:09 -0600

    Prefabs and Models
    Basic Token Prefab and Models with probuilder

---
+++ Assets/Recollection Snooker/Codes/Tokens/Die.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dante.RecollectionSnooker
+{
+    #region Enums
+
+    #endregion
+
+    #region Structs
+
+    #endregion
+
+    public class Die : Token
+    {
+
+        #region Knobs
+
+        #endregion
+
+        #region References
+
+        #endregion
+
+        #region Runtime Variables
+
+        #endregion
+
+        #region Unity Methods
+        void Start()
+        {
+
+        }
+
+
+        #endregion
+
+        #region Runtime Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Getters and Setters
+
+        #endregion
+    }
+
+}
